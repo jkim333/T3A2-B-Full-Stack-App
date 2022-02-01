0043f56f852be49758f7bcf04a9e52daefe68cb2
jkim333 <kimjihyung3@gmail.com>
2022-02-01 23:56:06 +1100

    Add styling to signup page.

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 // import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { Routes, Route, Link } from "react-router-dom";
 import Mainhome from "./pages/Home";
 
@@ -9,7 +10,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Mainhome />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
