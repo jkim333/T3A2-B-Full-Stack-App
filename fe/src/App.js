7ee587aa7308ecfd0048a3018de3f6bfd19f626b
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-02-03 01:15:19 +1100

    fe/exercise-page

--- fe/src/App.js
+++ fe/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from "./pages/Login";
 import { Routes, Route, Link } from "react-router-dom";
 import Mainhome from './pages/Home';
+import Search from './pages/Exercise';
 
 
 
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Mainhome/>} />
       <Route path="/login" element={<Login />} />
+      <Route path="/search-exercise" element={<Search />} />
     </Routes>
   </>
   
---
+++ fe/src/pages/Exercise.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Exercise from '../components/exercise';
+
+
+const Search = ()=>{
+    return(
+    <div >
+     <Exercise/>
+    </div>
+    )
+}
+
+export default Search;
