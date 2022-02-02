d56e976c9ef7bc1056867c2621450985b7b11456
Shriya Dhar <68209878+inverseswirl@users.noreply.github.com>
2022-02-03 01:21:15 +1100

    fe/exercise-page (#10)

--- fe/src/App.js
+++ fe/src/App.js
@@ -4,18 +4,24 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Routes, Route, Link } from "react-router-dom";
 import Mainhome from './pages/Home';
+import Search from './pages/Exercise';
 import Log from './pages/Log';
+
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Mainhome />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/log-entry" element={<Log />} />
-      </Routes>
-    </>
+
+    <Routes>
+      <Route path="/" element={<Mainhome/>} />
+      <Route path="/login" element={<Login />} />
+       <Route path="/signup" element={<Signup />} /> 
+       <Route path="/search-exercise" element={<Search />} />
+       <Route path="/log-entry" element={<Log />} />
+    </Routes>
+  </>
+  
+
   );
 }
 
