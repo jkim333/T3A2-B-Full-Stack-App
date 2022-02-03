6c019abeb96097fff6929a4ede673e61057ef8a3
Shriya Dhar <68209878+inverseswirl@users.noreply.github.com>
2022-02-03 20:00:46 +1100

    fe/calendar (#13)

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Routes, Route, Link } from "react-router-dom";
+import CalendarDisplay from './pages/Calendar';
 import Mainhome from "./pages/Home";
 import Search from "./pages/Exercise";
 import Log from "./pages/Log";
@@ -11,6 +12,7 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <>
+
       <Routes>
         <Route path="/" element={<Mainhome />} />
         <Route path="/login" element={<Login />} />
@@ -18,8 +20,10 @@ function App() {
         <Route path="/log-entry" element={<Log />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/search-exercise" element={<Search />} />
+        <Route path="/calendar" element={<CalendarDisplay />} />
       </Routes>
     </>
+
   );
 }
 
