020af689a7cad718c9149e9be23008f10855c429
Jihyung Kim <69041975+jkim333@users.noreply.github.com>
2022-02-03 16:38:07 +1100

    Create dashboard page. (#11)

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Routes, Route, Link } from "react-router-dom";
-import Mainhome from './pages/Home';
-import Search from './pages/Exercise';
-import Log from './pages/Log';
-
+import Mainhome from "./pages/Home";
+import Search from "./pages/Exercise";
+import Log from "./pages/Log";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
-
-    <Routes>
-      <Route path="/" element={<Mainhome/>} />
-      <Route path="/login" element={<Login />} />
-       <Route path="/signup" element={<Signup />} /> 
-       <Route path="/search-exercise" element={<Search />} />
-       <Route path="/log-entry" element={<Log />} />
-    </Routes>
-  </>
-  
-
+      <Routes>
+        <Route path="/" element={<Mainhome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/log-entry" element={<Log />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/search-exercise" element={<Search />} />
+      </Routes>
+    </>
   );
 }
 
