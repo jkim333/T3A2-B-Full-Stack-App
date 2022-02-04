f22bea56d4d35315d4139e993a02fca9cd1c1100
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-02-04 21:24:48 +1100

    be/csv-import

--- be/app.js
+++ be/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require("cors");
+const csv = require('csv-parser');
+const fs = require("fs");
+const helmet = require("helmet");
 const port = 3002;
 
+app.use(cors());
+app.use(helmet());
+
+const results = []
+fs.createReadStream('./csv.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () =>  {
+            JSON.stringify(results);
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(results);
 });
 
 mongoose.connect('mongodb://root:password@mongo:27017/db?authSource=admin')
