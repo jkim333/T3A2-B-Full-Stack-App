aff3be79655f9aa3c727897692a09356722914b1
jkim333 <kimjihyung3@gmail.com>
2022-02-06 12:59:59 +1100

    Create authentication using JWT.

---
+++ be/controllers/auth.js
@@ -0,0 +1,73 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+const user = require("../models/user");
+
+module.exports.postLogin = (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ error: "Incorrect username or password." });
+      }
+
+      return bcrypt.compare(password, user.password).then((match) => {
+        if (match) {
+          const userData = { userId: user._id.toString() };
+          const accessToken = jwt.sign(
+            userData,
+            process.env.ACCESS_TOKEN_SECRET
+          );
+          return res.json({ accessToken: accessToken });
+        }
+        return res
+          .status(401)
+          .json({ error: "Incorrect username or password." });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: "Something went wrong with the server." });
+    });
+};
+
+module.exports.postSignup = (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (user) {
+        return res.status(409).json({
+          error: `A user with the username '${username}' already exists.`,
+        });
+      }
+
+      return bcrypt.hash(password, 10).then((hashedPassword) => {
+        const user = new User({ username: username, password: hashedPassword });
+        return user.save().then(() => {
+          res
+            .status(201)
+            .json({ message: "A new user was successfully created." });
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: "Something went wrong with the server." });
+    });
+};
+
+module.exports.getAuthenticated = (req, res, next) => {
+  console.log(req.user);
+  return res.json({ message: "hello world" });
+};
