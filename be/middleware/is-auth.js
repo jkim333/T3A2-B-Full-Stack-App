9649d597faa2360eb469d70c7170df13712a4230
Jihyung Kim <69041975+jkim333@users.noreply.github.com>
2022-02-07 08:35:55 +1100

    Be/authentication (#16)
    * Create dashboard page.
    
    * Create authentication using JWT.
    
    Co-authored-by: Shriya Dhar <68209878+inverseswirl@users.noreply.github.com>

---
+++ be/middleware/is-auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "Unauthorised request." });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: "Invalid token." });
+    req.user = user;
+    next();
+  });
+};
