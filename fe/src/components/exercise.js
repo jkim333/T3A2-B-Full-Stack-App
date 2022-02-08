7c8c357bfa19d869bc0d8ef7890c972fbc1739be
Shriya Dhar <68209878+inverseswirl@users.noreply.github.com>
2022-02-08 22:34:10 +1100

    handledata function (#29)

--- fe/src/components/exercise.js
+++ fe/src/components/exercise.js
@@ -2,18 +2,13 @@ import React, { useState, useEffect} from 'react';
 import Navbar from './navbar';
 import Input from './loginput';
 
-
-
-
 const ExerciseType = ({type,exercises, handleBtn,btn})=>{
-
   const [allexercises, setActivities] = useState([])
   const [activityBtn, handleActivity] = useState(false)
   const[id, handleActivityid] =useState("")
   const [exercise, setExercise] = useState("")
   const [activity, setActivity] = useState("")
  
-
   function handleExercisevalue(e){
        e.preventDefault()
        const new_exercises = exercises.filter((exercise)=> exercise.exercise === type)
@@ -23,18 +18,17 @@ const ExerciseType = ({type,exercises, handleBtn,btn})=>{
       }
       
  
-
-        function handleActivityvalue(e){
-           e.preventDefault()
-          handleActivity(true)
-          setActivity(e.target.value)
-          allexercises.map((element)=>{
-            if(element.activity === e.target.value){
-                handleActivityid(element._id)
-            }
-            return id
-          })
+    function handleActivityvalue(e){
+      e.preventDefault()
+      handleActivity(true)
+      setActivity(e.target.value)
+      allexercises.map((element)=>{
+        if(element.activity === e.target.value){
+            handleActivityid(element._id)
         }
+        return id
+      })
+    }
         
       
       if(activityBtn){
@@ -77,52 +71,59 @@ const TitleBar = ()=>{
        )
 }
   
+
+
+
+
 const ExerciseDisplay = ()=>{
 
   const [exercises, handleExercises] = useState([])
   const [btn, handleBtn] = useState(false)
 
-
   useEffect(() => {
-    
     async function fetchData(){
       try{
           let exercise_response = await fetch("http://localhost:3002/exercises")
           let data = await exercise_response.json();
           handleExercises(data.results)
+
         }catch(err){
           alert(err);
         }
       }
-        fetchData()
-    }, [])
-    
-   const new_array = []
-   exercises.map((exercise)=>{
-    return new_array.push(exercise.exercise)
-   })
-
-   let first_occurence = false
-   let final_array = []
-   for(let i=0;i<new_array.length-1;i++){
-      if(new_array[i] === new_array[i + 1]  && first_occurence === false){
-          first_occurence = true
-          final_array.push(new_array[i])
-        }
-
-        else if (new_array[i] !== new_array[i + 1]){
-              final_array.push(new_array[i + 1])
-        }
+         fetchData()
     }
+    ,[])
 
-
+    function handledata(exercises){
+        const new_array = []
+        exercises.map((exercise)=>{
+         return new_array.push(exercise.exercise)
+        })
+      
+        let first_occurence = false
+        let final_array = []
+        for(let i=0;i<new_array.length-1;i++){
+           if(new_array[i] === new_array[i + 1]  && first_occurence === false){
+               first_occurence = true
+               final_array.push(new_array[i])
+             }
+      
+             else if (new_array[i] !== new_array[i + 1]){
+                   final_array.push(new_array[i + 1])
+             }
+         }
+         return final_array
+        }
 
   return(
+    
+
   <div className='w-screen h-screen overflow-hidden box-border  bg-gradient-to-b from-blue-900 to-sky-800'>
    <Navbar/>
    <TitleBar/>
    {
-     final_array.map((type,index)=>
+     handledata(exercises).map((type,index)=>
      <div key={index}>
      <ExerciseType 
      type={type} 
