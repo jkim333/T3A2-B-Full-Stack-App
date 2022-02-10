eb27307feacdcc0d14c2f8446639205e060a1695
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-02-10 21:56:21 +1100

    redirect to home after data fetch

--- fe/src/pages/Log.js
+++ fe/src/pages/Log.js
@@ -1,13 +1,29 @@
 import React from "react";
-import Input from "../components/loginput";
 import Navbar from "../components/navbar";
+import {Table} from '../components/table';
+import {Link} from 'react-router-dom';
 
-const Log = () => {
-  return (
-    <div className="w-screen h-screen overflow-hidden box-border  bg-gradient-to-b from-blue-900 to-sky-800">
-      <Navbar />
-      <Input />
-    </div>
-  );
-};
+
+const Addbutton = () => {
+    return (
+      <>
+        <button className="text-blue-200 bg-sky-800 opacity-75 hover:bg-sky-700 rounded-full w-16 h-16 text-4xl cursor-pointer mt-40 mb-5 mx-auto block">
+          <Link to="/search-exercise">+</Link>
+        </button>
+        <p className="text-blue-200 text-center p-0 m-0">Start new workout</p>
+      </>
+    );
+  };
+
+const Log =()=>{
+    return(
+        <div className="w-screen h-screen overflow-hidden box-border  bg-gradient-to-b from-blue-900 to-sky-800">
+         <Navbar/>
+         <Table/>
+         <Addbutton/>
+
+        </div>
+    )
+}
+
 export default Log;
