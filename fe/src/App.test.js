e4c31f2d65d2d97977e23939e87f1b1fc7548f5c
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2022-02-11 17:09:46 +1100

    package changes

--- fe/src/App.test.js
+++ fe/src/App.test.js
@@ -1,15 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, screen, fireEvent} from '@testing-library/react';
 import App from './App';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar'
+import TestRenderer from 'react-test-renderer'; // ES6
+import Input from './components/input';
+import ExerciseDisplay from './components/exercise';
+import Search from './pages/Exercise';
+import WeightEntry from './components/input'
+import Mainhome from './pages/Home';
 
 
 
 
+//brandname
+// test('renders the Xero fitness brand name', () => {
+//     render(<Navbar/>)
+//     const element = screen.getByTestId('brandname')
+//     expect(element).toHaveTextContent('Xero fitness');
+// })
 
-//home page
 
-test('renders the Xero fitness brand name', () => {
-    const{getByText} = render(<Navbar/>)
-    const brandname = getByText(/Xero fitness/)
-    expect(brandname).toBeInTheDocument();
-})
+// function fetchData(){
+//     const result = await fetch("http://localhost:3002/")
+//     const data = await result.json()
+//     return data
+// }
+
+// test('fetches exercise list',()=>{
+//      const data = await fetchData()
+//      expect(data).toContain('Abs')
+     
+// })
+it('renders correctly', () => {
+    const tree = TestRenderer
+      .create(<Search/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Input should not be null', () => {
+      render(<Mainhome/>)
+    const input = screen.getByTestId('weight-input')
+    fireEvent.change(input)
+    expect(input.value).not.toBeNull();
+  })
